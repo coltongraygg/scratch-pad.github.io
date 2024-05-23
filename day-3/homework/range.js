8f84a33a4df35fb40343309c61181a8a1688170d
coltongraygg <163790834+coltongraygg@users.noreply.github.com>
2024-05-23 04:51:40 +0000

    range.js completed

--- day-3/homework/range.js
+++ day-3/homework/range.js
@@ -21,12 +21,42 @@
  *  b. you'll need a loop, which one is best?
  *  c. you'll wanna make use of the push() method of Array.
  */
+
+/* 
+I: FUNCTION RECEIVES TWO NUMERICAL VALUES
+O: FUNCTION OUTPUTS ALL INTERGERS INBETWEEN THE TWO NUMERICAL VALUES, IF NUM 1 > NUM 2 - REVERSE ORDER
+C: FUNCTION EXPECTS TO RECEIVE 2 NUMERICAL VALUES
+E: N/A
+*/
+
 function range(start, end) {
     // YOUR CODE GOES BELOW HERE //
-    
-    
-    
-    
+
+    // create storageArr 
+    let storageArr = []; 
+
+    // create if statement: start param is <= end param 
+    if (start <= end) {
+        // while state param is <= end param, loop
+        while (start <= end) {
+            // push start to storageArr
+            storageArr.push(start);
+            // increase the numerical value of start by 1
+            start++;
+        }
+        // create else if statement: start param is >= end param
+        } else if (start >= end) {
+            // while start param is >= end param, loop
+            while (start >= end) {
+                // push start to storageArr
+                storageArr.push(start);
+                // decrease the numerical value of start by 1
+                start--;
+            }
+        // return the storageArr
+        } return storageArr
+
+  
     // YOUR CODE GOES ABOVE HERE //
 }
 
